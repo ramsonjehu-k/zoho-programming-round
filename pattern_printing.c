3e06c5651e9040bd50dace4bc037f5eb24a0bbbe
Ramson Jehu K <72766992+ramsonjehu-k@users.noreply.github.com>
2021-02-06 09:50:12 +0530

    initial commit
    The solution for the Level 1 programming round.

---
+++ pattern_printing.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    int input;
+    int row,col,val;
+    scanf("%d",&input);            // getting input value
+    val = input;
+    
+    for(row=0;row<input;row++){    // for printing rows
+        for(col=0;col<input;col++) // for printing columns
+        {
+            printf("%d",val);
+            if(col<row){          
+                val--;
+            }
+        }
+        val = input;
+        printf("\n");
+    }
+    return 0;
+}
